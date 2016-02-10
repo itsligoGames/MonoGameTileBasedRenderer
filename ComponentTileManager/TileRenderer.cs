b2db8bcf911884694076a3cb81aac4bfb42e9085
ITSPaul <powell.paul@itsligo.ie>
2016-02-10 20:03:55 +0000

    Coded Random placed enemies tht will follow the player. Uses Linq queries on Tile Collection to position enemies randomly

--- ComponentTileManager/TileRenderer.cs
+++ ComponentTileManager/TileRenderer.cs
@@ -28,6 +28,8 @@ namespace ComponentTileManager
         List<TILETYPES> _tileTypes = new List<TILETYPES>();
         List<TILETYPES> _nonPassableTiles = new List<TILETYPES>();
         Vector2 TileTransform;
+        List<RotatingSprite> _enemies = new List<RotatingSprite>();
+
         public TileManager TileManager
         {
             get
@@ -70,8 +72,42 @@ namespace ComponentTileManager
             _tileManager.ActiveLayer = _tileManager.getLayer("background");
             _tileManager.ActiveLayer.makeImpassable(impassibleTiles);
             _tileManager.CurrentTile = _tileManager.ActiveLayer.Tiles[0,0];
+            // Setup the collision objects for the layer
             setupCollisionMask();
+            // Create an enemy object that will rotate towards the player
+            //Tile enemyTile = _tileManager.ActiveLayer.Impassable.First();
+            setupEnemies(20, tileWidth, tileHeight);
         }
+
+        public void setupEnemies(int EnemyCount, int tileWidth, int tileHeight)
+        {
+            // use linq queries on the impassible tiles to choose random locations for enemies
+            // First query introduce a random guid into a sub set of locations of impassible tiles
+            var enemyPlaces = _tileManager.ActiveLayer.Impassable
+                                    .Select(subset => new { subset.X, subset.Y, gid = Guid.NewGuid() });
+            // order by the guid and take count positions
+            // NOTE: Linq ensures that there are no duplicate positions produced
+            var randomEnemyPlaces = enemyPlaces.Select(all => new { all.gid, all.X, all.Y })
+                                        .OrderBy(subgroup => subgroup.gid).Take(EnemyCount)
+                                        //.ToList().Select(r => new { r.X, r.Y })
+                                        .ToList();
+
+            // Do a join on the resulting 10 random places and the original impassible tiles 
+            // to get the actual tile locations
+
+            List<Tile> enemyPositions = (from enemyPos in _tileManager.ActiveLayer.Impassable
+                                         join places in randomEnemyPlaces
+                                         on new { enemyPos.X, enemyPos.Y } equals new { places.X, places.Y }
+                                         select enemyPos).ToList();
+            foreach (Tile t in enemyPositions)
+            {
+                _enemies.Add(new RotatingSprite(new Vector2(t.X, t.Y),
+                    new List<TileRef>() {
+                    new TileRef(17,7,0)
+                         }, tileWidth, tileHeight, 1f));
+            }
+        }
+
 
         public void setupCollisionMask()
         {
@@ -92,6 +128,11 @@ namespace ComponentTileManager
             {
                 _player.Update(gameTime);
                 _player.pixelMove(_collisionSet);
+                foreach (var _enemy in _enemies)
+                {
+                    _enemy.follow(_player);
+                    _enemy.Update(gameTime);
+                }
                 Camera Cam = Game.Services.GetService<Camera>();
                 Cam.follow(_player.PixelPosition,
                  GraphicsDevice.Viewport);
@@ -100,40 +141,40 @@ namespace ComponentTileManager
             base.Update(gameTime);
         }
 
-        private Tile getOverlappingTile()
-        {
-            foreach (Tile t in _tileManager.ActiveLayer.Tiles)
-            {
-                Rectangle tRect = new Rectangle(new Point(t.X * t.TileWidth, t.Y * t.TileHeight),
-                    new Point(t.TileWidth, t.TileHeight));
-                if (_player.DrawRectangle.Intersects(tRect) && !t.Passable)
-                    return t;
-            }
-            return null;
-        }
+        //private Tile getOverlappingTile()
+        //{
+        //    foreach (Tile t in _tileManager.ActiveLayer.Tiles)
+        //    {
+        //        Rectangle tRect = new Rectangle(new Point(t.X * t.TileWidth, t.Y * t.TileHeight),
+        //            new Point(t.TileWidth, t.TileHeight));
+        //        if (_player.DrawRectangle.Intersects(tRect) && !t.Passable)
+        //            return t;
+        //    }
+        //    return null;
+        //}
 
-        private bool passable(DIRECTION direction, Tile currentPlayerTile, List<Tile> nonpassable)
-        {
-            if (_player.MovingState == STATE.MOVING && _player.Direction == DIRECTION.DOWN
-                && !_tileManager.ActiveLayer.getadjacentTile("below", _player.CurrentPlayerTile).Passable)
-                _player.MovingState = STATE.STILL;
+        //private bool passable(DIRECTION direction, Tile currentPlayerTile, List<Tile> nonpassable)
+        //{
+        //    if (_player.MovingState == STATE.MOVING && _player.Direction == DIRECTION.DOWN
+        //        && !_tileManager.ActiveLayer.getadjacentTile("below", _player.CurrentPlayerTile).Passable)
+        //        _player.MovingState = STATE.STILL;
 
-            else if (_player.MovingState == STATE.MOVING && _player.Direction == DIRECTION.UP
-                && !_tileManager.ActiveLayer.getadjacentTile("above", _player.CurrentPlayerTile).Passable)
-                _player.MovingState = STATE.STILL;
+        //    else if (_player.MovingState == STATE.MOVING && _player.Direction == DIRECTION.UP
+        //        && !_tileManager.ActiveLayer.getadjacentTile("above", _player.CurrentPlayerTile).Passable)
+        //        _player.MovingState = STATE.STILL;
 
-            else if (_player.MovingState == STATE.MOVING && _player.Direction == DIRECTION.LEFT
-                && !_tileManager.ActiveLayer.getadjacentTile("left", _player.CurrentPlayerTile).Passable)
-                _player.MovingState = STATE.STILL;
+        //    else if (_player.MovingState == STATE.MOVING && _player.Direction == DIRECTION.LEFT
+        //        && !_tileManager.ActiveLayer.getadjacentTile("left", _player.CurrentPlayerTile).Passable)
+        //        _player.MovingState = STATE.STILL;
 
-            else if (_player.MovingState == STATE.MOVING && _player.Direction == DIRECTION.RIGHT
-                && !_tileManager.ActiveLayer.getadjacentTile("right", _player.CurrentPlayerTile).Passable)
-                _player.MovingState = STATE.STILL;
+        //    else if (_player.MovingState == STATE.MOVING && _player.Direction == DIRECTION.RIGHT
+        //        && !_tileManager.ActiveLayer.getadjacentTile("right", _player.CurrentPlayerTile).Passable)
+        //        _player.MovingState = STATE.STILL;
 
-            if (_player.MovingState == STATE.MOVING)
-                return true;
-            else return false;
-        }
+        //    if (_player.MovingState == STATE.MOVING)
+        //        return true;
+        //    else return false;
+        //}
 
         public void AddPlayer(PlayerWithWeapon p)
         {
@@ -161,10 +202,12 @@ namespace ComponentTileManager
             }
             if (_player != null) {
                 _player.Draw(sp, _tileSheet);
-                if (_player.MyProjectile != null)
-                sp.DrawString(font, "ptp " + _player.Tileposition.ToString(), new Vector2(10, 10), Color.White);
-                sp.DrawString(font, "prtp " + _player.MyProjectile.Tileposition.ToString(), new Vector2(10, 30), Color.White);
-                sp.DrawString(font, "stp "+ _player.Site.Tileposition.ToString(), new Vector2(10, 60), Color.White);
+                foreach (var _enemy in _enemies)
+                    _enemy.Draw(sp, _tileSheet);
+                //if (_player.MyProjectile != null)
+                //    sp.DrawString(font, "ptp " + _player.Tileposition.ToString(), new Vector2(10, 10), Color.White);
+                //    sp.DrawString(font, "prtp " + _player.MyProjectile.Tileposition.ToString(), new Vector2(10, 30), Color.White);
+                //    sp.DrawString(font, "stp "+ _player.Site.Tileposition.ToString(), new Vector2(10, 60), Color.White);
             }
             foreach (var item in _collisionSet)
             {
